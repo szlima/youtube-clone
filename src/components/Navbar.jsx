4306d507e8a7ed6d260cf6df283f7c1230ebe58d
szlima <suzanelima@outlook.com>
2023-08-02 16:23:57 -0300

    Inserindo a logo na navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ export default function Navbar(){
     return (
         <>
             <nav className="navbar">
-
+                <div className='navbar-toggle'>
+                    <img src='../src/img/logo.png' alt='Logo do Youtube'/>
+                </div>
             </nav>            
         </>
     );
