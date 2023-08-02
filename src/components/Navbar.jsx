e1cd203486c60560e97ddee3e9b8a26a3a1368d5
szlima <suzanelima@outlook.com>
2023-08-02 17:21:34 -0300

    Exibindo ícone de menu esquerdo para telas maiores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ export default function Navbar(){
         <>
             <nav className="navbar">
                 <div className='navbar-toggle'>
+                    <ion-icon name="menu-outline"></ion-icon>
                     <img src='../src/img/logo.png' alt='Logo do Youtube'/>
                 </div>
             </nav>            
