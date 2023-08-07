1aef96d47417a683e6c090320283c01e05a1a7f5
szlima <suzanelima@outlook.com>
2023-08-07 12:42:10 -0300

    Exibindo botão de volta ao início para vídeo indisponível em telas maiores

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -11,7 +11,7 @@ export default function VideoPage({video}){
                 <main className="videopage-unavailable">
                     <img src='../src/img/unavailable_video.png' alt='Vídeo não encontrado'/>
                     <h1>Este vídeo não está mais disponível</h1>
-                    
+                    <button>Ir para a página inicial</button>
                 </main>
                 }
             </div>
