663c16357855a091dcb881580630edec87264fc1
szlima <suzanelima@outlook.com>
2023-08-07 12:06:47 -0300

    Exibindo mensagem para vídeos indisponíveis

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,10 +1,20 @@
 import Navbar from "./Navbar";
 
-export default function VideoPage(){
+export default function VideoPage({video}){
 
     return (
         <>
             <Navbar />
+            <div className="videopage">
+                {video ? 'video'
+                :
+                <main className="videopage-unavailable">
+                    
+                    <h1>Este vídeo não está mais disponível</h1>
+                    
+                </main>
+                }
+            </div>
         </>
     );
 }
