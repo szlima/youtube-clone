8609b50ede56ab8de9120dd64d8af4a2604ed0aa
szlima <suzanelima@outlook.com>
2023-08-08 18:48:01 -0300

    Criando área de reprodução para vídeos disponíveis

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -6,13 +6,16 @@ export default function VideoPage({video}){
         <>
             <Navbar />
             <div className="videopage">
-                {video ? 'video'
+                {video ?
+                    <div className="videopage-available">
+                        {video}
+                    </div>
                 :
-                <main className="videopage-unavailable">
-                    <img src='../src/img/unavailable_video.png' alt='Vídeo não encontrado'/>
-                    <h1>Este vídeo não está mais disponível</h1>
-                    <button>Ir para a página inicial</button>
-                </main>
+                    <main className="videopage-unavailable">
+                        <img src='../src/img/unavailable_video.png' alt='Vídeo não encontrado'/>
+                        <h1>Este vídeo não está mais disponível</h1>
+                        <button>Ir para a página inicial</button>
+                    </main>
                 }
             </div>
         </>
