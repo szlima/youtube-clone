27a3bc8f1bf99e0705be4410ed14d32a15e80c51
szlima <suzanelima@outlook.com>
2023-08-21 14:06:02 -0300

    Exibindo informações sobre cada vídeo relacionado

--- src/components/CompactVideo.jsx
+++ src/components/CompactVideo.jsx
@@ -4,6 +4,7 @@ import {getInfoDate} from '../utils/functions';
 export default function CompactVideo(){
 
     const video= {
+        url: '#',
         title: 'Hino Oficial | JMJ Lisboa 2023 | Vídeo Oficial',
         channel: 'Lisboa 2023',
         views: '1 mi de visualizações',
@@ -21,7 +22,14 @@ export default function CompactVideo(){
             <div className='compact-video-info'>
                 <div className='compact-video-info-logo'></div>
                 <div className='compact-video-info-text'>
-                                      
+                    <a href={video.url} className='compact-video-info-text-title'>{video.title}</a>
+                    <div className='compact-video-info-text-data'>
+                        <span className='compact-video-info-text-data-channel'>{video.channel}</span>
+                        <span className='compact-video-info-text-data-info'>
+                            <span className='compact-video-info-text-data-info-views'>{video.views}</span>
+                            <span className='compact-video-info-text-data-info-publishedAt'>{getInfoDate(video.publishedAt)}</span>
+                        </span>
+                    </div>
                 </div>
                 <ion-icon name="ellipsis-vertical-outline"></ion-icon>
             </div>
