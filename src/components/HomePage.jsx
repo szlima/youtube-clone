5f66c071e308c9b248a7d8122348130b16a3ce88
szlima <suzanelima@outlook.com>
2023-09-13 16:22:19 -0300

    Definindo a exibição do menu lateral em telas maiores

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage(){
             <div className='homepage'>
                 <nav className='homepage-navbar'>
                     <div className='homepage-navbar-section'>
-                        <div className='homepage-navbar-section-option'>
+                        <div className='homepage-navbar-section-option active-navbar'>
                             <ion-icon name="home-sharp"></ion-icon>
                             <p>Início</p>
                         </div>
@@ -18,7 +18,7 @@ export default function HomePage(){
                             <p>Shorts</p>
                         </div>
                         <div className='homepage-navbar-section-option'>
-                            <ion-icon name="play"></ion-icon>
+                            <ion-icon name="play-outline"></ion-icon>
                             <p>Inscrições</p>
                         </div>
                     </div>
@@ -33,22 +33,110 @@ export default function HomePage(){
                         </div>
                     </div>
                     <div className='homepage-navbar-section'>
-                        login
+                        <p>Faça login para curtir vídeos, comentar e se inscrever.</p>
+                        <button>
+                            <ion-icon name="person-circle-outline"></ion-icon>
+                            <span>Fazer login</span>
+                        </button>
                     </div>
                     <div className='homepage-navbar-section'>
-                        explorar
+                        <h3>Explorar</h3>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="flame-outline"></ion-icon>
+                            <p>Em alta</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="bag-handle-outline"></ion-icon>
+                            <p>Shopping</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="musical-note-outline"></ion-icon>
+                            <p>Música</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="videocam-outline"></ion-icon>
+                            <p>Filmes</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="radio-outline"></ion-icon>
+                            <p>Ao vivo</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="game-controller-outline"></ion-icon>
+                            <p>Jogos</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="newspaper-outline"></ion-icon>
+                            <p>Notícias</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="trophy-outline"></ion-icon>
+                            <p>Esportes</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="bulb-outline"></ion-icon>
+                            <p>Aprender</p>
+                        </div>
                     </div>
                     <div className='homepage-navbar-section'>
-                        procurar
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                            <p>Procurar canais</p>
+                        </div>
                     </div>
                     <div className='homepage-navbar-section'>
-                        mais
+                        <h3>Mais do YouTube</h3>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="logo-youtube"></ion-icon>
+                            <p>Youtube Premium</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="logo-youtube"></ion-icon>
+                            <p>Youtube Music</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="logo-youtube"></ion-icon>
+                            <p>Youtube Kids</p>
+                        </div>
                     </div>
                     <div className='homepage-navbar-section'>
-                        configurações
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="settings-outline"></ion-icon>
+                            <p>Configurações</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="flag-outline"></ion-icon>
+                            <p>Histórico de denúncias</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="help-circle-outline"></ion-icon>
+                            <p>Ajuda</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="information-circle-outline"></ion-icon>
+                            <p>Enviar feedback</p>
+                        </div>
                     </div>
                     <div className='homepage-navbar-section'>
-                        sobre
+                        <div className='homepage-navbar-section-option'>
+                            <a href='#'>Sobre</a>
+                            <a href='#'>Imprensa</a>
+                            <a href='#'>Direitos autorais</a>
+                            <a href='#'>Entre em contato</a>
+                            <a href='#'>Criadores de conteúdo</a>
+                            <a href='#'>Publicidade</a>
+                            <a href='#'>Desenvolvedores</a>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <a href='#'>Termos</a>
+                            <a href='#'>Privacidade</a>
+                            <a href='#'>Política e segurança</a>
+                            <a href='#'>Como funciona o Youtube</a>
+                            <a href='#'>Testar os novos recursos</a>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <p>© 2023 Google LLC</p>
+                        </div>
                     </div>
                 </nav>
                 <main className='homepage-main'>
