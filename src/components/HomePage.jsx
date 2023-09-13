96b3f4cc149d9065b32198acdf4e43f957ba2884
szlima <suzanelima@outlook.com>
2023-09-13 11:24:53 -0300

    Definindo as seções do menu lateral

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,7 +8,48 @@ export default function HomePage(){
             
             <div className='homepage'>
                 <nav className='homepage-navbar'>
-                    nav
+                    <div className='homepage-navbar-section'>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="home-sharp"></ion-icon>
+                            <p>Início</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="caret-forward-circle-outline"></ion-icon>
+                            <p>Shorts</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="play"></ion-icon>
+                            <p>Inscrições</p>
+                        </div>
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="folder-open-outline"></ion-icon>
+                            <p>Biblioteca</p>
+                        </div>
+                        <div className='homepage-navbar-section-option'>
+                            <ion-icon name="timer-outline"></ion-icon>
+                            <p>Histórico</p>
+                        </div>
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        login
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        explorar
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        procurar
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        mais
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        configurações
+                    </div>
+                    <div className='homepage-navbar-section'>
+                        sobre
+                    </div>
                 </nav>
                 <main className='homepage-main'>
                     main
